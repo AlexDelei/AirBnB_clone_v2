167f2c20ec60f24e61ae19541b658eece866c9e2
AlexDelei <alexdelei125@gmail.com>
2024-03-02 22:53:41 -0800

    Fixed the connection of states table and cities table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if STORAGE_TYPE == 'db':
+        __tablename__ = 'states'
+        id = Column(String(60), primary_key=True, nullable=False)
+        name = Column(String(128), nullable=False)
+
+
+cities = relationship("City", back_populates="state", cascade="delete")
