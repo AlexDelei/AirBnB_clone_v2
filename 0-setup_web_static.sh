2ad9f7a622807241cb6e6b920f87d285654e0974
AlexDelei <alexdelei125@gmail.com>
2024-03-07 17:31:54 -0800

    a scri[t for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
-# Deploying into my webserver web-01
+# sets up my web servers for the deployment of web_static
+
+echo -e "\e[1;32m START\e[0m"
+
+#--Updating the packages
 sudo apt-get -y update
 sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
 
-# creating the necessary folders
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+#--configure firewall
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
+echo
 
-# A fake html file for testing
-echo "<html><head></head><body>Testing mic 1.2</body></html>" | sudo tee /data/web_static/releases/test/index.html
+#--created the dir
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo
 
-# creating or recreating the symbolic link
+#--adds test string
+echo "<h1>Welcome to www.gbetisolutions.tech</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
+
+#--prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
+
+#--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
 
-# Changing ownership
-sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-# updating nginx configuration
-config_text="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
-sudo sh -c "echo '$config_text' > /etc/nginx/sites-available/default"
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
 
-# then restarting nginx
+#--restart NGINX
 sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"	
