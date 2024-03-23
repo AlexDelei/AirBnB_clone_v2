e49c70cbf86c195f7e7b8ee01f41bd897e11815a
AlexDelei <alexdelei125@gmail.com>
2024-03-23 21:59:23 +0000

    review feature

--- models/review.py
+++ models/review.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+import os
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    if STORAGE_TYPE != 'db':
+        place_id = ""
+        user_id = ""
+        text = ""
