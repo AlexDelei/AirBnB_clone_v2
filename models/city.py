4ff711450f50886f1f0b58cdbfe60e9a267753ae
AlexDelei <alexdelei125@gmail.com>
2024-03-23 21:26:37 +0000

    a little change on relationship

--- models/city.py
+++ models/city.py
@@ -11,12 +11,13 @@ STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base):
     """City class handles all application cities"""
-    if STORAGE_TYPE == 'db':
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        states = relationship("State", back_populates='cities')
-    else:
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade='delete')
+    states = relationship("State", back_populates='cities')
+
+    if STORAGE_TYPE != "db":
         state_id = ''
         name = ''
         places = []
