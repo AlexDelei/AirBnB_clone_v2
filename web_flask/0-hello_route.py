84d8ab1c0c6861f69889af73e22a9ae409ff216d
AlexDelei <alexdelei125@gmail.com>
2024-03-23 07:01:47 +0000

    hello flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from flask import Flask
 """
 A simple demostration of creating a flask web app
 using the the flask framework
 """
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
