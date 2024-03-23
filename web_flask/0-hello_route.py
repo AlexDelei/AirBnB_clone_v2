9db76c999b468750ccb1cd10912f5e293315ca6a
AlexDelei <alexdelei125@gmail.com>
2024-03-23 08:20:28 +0000

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-	"""
-	Return hello hbnb
-	"""
-	return 'Hello HBNB!'
+    """
+    Return hello hbnb
+    """
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
