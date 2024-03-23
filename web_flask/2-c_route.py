a8d0812f23e622a5a41ad0d11818cd55e13e8628
AlexDelei <alexdelei125@gmail.com>
2024-03-23 08:16:03 +0000

    printout the url params

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,19 +7,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-	"""Return hello hbnb"""
-	return 'Hello HBNB!'
+    """Return hello hbnb"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""Return hbnb"""
-	return 'HBNB'
+    """Return hbnb"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def routing(text):
-	"""printout the text passed as parameter"""
-	return 'C {}'.format(text.replace('_', ' '))
+    """printout the text passed as parameter"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
