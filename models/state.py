23016808845a7153af71a8cc4152ba601029fe52
AlexDelei <alexdelei125@gmail.com>
2024-03-24 12:07:53 +0000

    list everything

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 import os
 from models.base_model import BaseModel
 from models.base_model import Base
@@ -12,22 +13,24 @@ STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """ State class """
-    if STORAGE_TYPE == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", back_populates="states", cascade="delete")
-    else:
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if STORAGE_TYPE != "db":
         name = ''
 
         @property
-        def list_cities(self):
+        def cities(self):
             """
             returns a list of Cities which are related
             to this state
             """
             temp = []
-            for city in models.storage.all("City").values():
+            for city in models.storage.all(models.city.City).values():
                 # only instances with the current state id
                 if city.state_id == self.id:
                     temp.append(city)
             return temp
+
+
+cities = relationship("City", back_populates="states", cascade="delete")
