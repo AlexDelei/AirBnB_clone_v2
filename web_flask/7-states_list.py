5ff92661db9bbeaa2bffe268e8006b83e0c3c435
AlexDelei <alexdelei125@gmail.com>
2024-04-17 10:08:21 +0000

    list zities

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Listing all the states on a webpage"""
-    st = sorted(storage.all(models.state.State).values(), key=lambda s: s.name)
+    st = storage.all(models.state.State)
     return render_template("7-states_list.html", states=st)
 
 
