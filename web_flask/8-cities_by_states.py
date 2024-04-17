c026e5f695a5d677310bc48427ccf24a1b9659bc
AlexDelei <alexdelei125@gmail.com>
2024-04-17 10:43:17 +0000

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to starts flask with name of states and cities"""
+import models
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_state/", strict_slashes=False)
+def display_states_cities():
+    """Returns cities by states for web"""
+    states = storage.all(models.state.State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def display_states_close(error):
+    """Closes session of program"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
