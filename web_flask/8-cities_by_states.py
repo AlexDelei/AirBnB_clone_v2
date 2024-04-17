ea5a2304d32cb73ab421625c4708f173c565c60d
AlexDelei <alexdelei125@gmail.com>
2024-04-17 12:57:24 +0000

    list city by stet

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""Module to starts flask with name of states and cities"""
+"""
+List all cities by their states
+"""
 import models
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_state/", strict_slashes=False)
-def display_states_cities():
-    """Returns cities by states for web"""
-    states = storage.all(models.state.State)
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def display_states_close(error):
-    """Closes session of program"""
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session after each request."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of states and cities."""
+    states = storage.all(models.state.State)
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
